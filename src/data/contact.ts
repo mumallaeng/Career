3c007c7f3362c1feeef2bc45b34f32ccb751c62a
mumallaeng <mumallaeng@outlook.com>
2026-08-12 18:23:46 +0900

    Link Hugging Face profile

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,3 +2,5 @@ export const contactEmail = 'mumallaeng@outlook.com';
 export const contactPhone = '010-4557-0214';
 export const contactGithub = 'mumallaeng';
 export const contactGithubUrl = `https://github.com/${contactGithub}`;
+export const contactHuggingFace = 'mumallaeng';
+export const contactHuggingFaceUrl = `https://huggingface.co/${contactHuggingFace}`;
